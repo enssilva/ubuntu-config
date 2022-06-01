d5224f1847720068d53e64c45253e90d5bb563c1
enssilva <ebenezer@duck.com>
2022-05-31 21:17:17 -0300

    fix to install apps with wine

---
+++ files/install-CNA.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+WINEARCH=win32 WINEPREFIX=/home/ebenezer/.wine wine jre-8u311-windows-i586.exe /s
+WINEARCH=win32 WINEPREFIX=/home/ebenezer/.wine wine cna-windows-k9-installer-6-3-4-en.exe
